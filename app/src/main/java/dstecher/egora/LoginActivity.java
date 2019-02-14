20376000e1eae2fff5299ffa16dc09713adc28ac
Nina <tietje.hh@gmail.com>
2019-02-14 22:53:56 +0100

    onclick listener
    ACHTUNG > MainActivity wird hier nicht mehr aufgerufen!
    
    Warnings only refers to layout(xml).

--- app/src/main/java/dstecher/egora/LoginActivity.java
+++ app/src/main/java/dstecher/egora/LoginActivity.java
@@ -1,16 +1,31 @@
 package dstecher.egora;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin123);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("login", "login button clicked");
+            }
+        });
+
     }
 }
